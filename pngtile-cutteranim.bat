44ca247312495794aada15c55dc14c3f7b4fdae2
Etherealxx <64251396+etherealxx@users.noreply.github.com>
2023-02-03 13:34:32 +0700

    first upload

---
+++ pngtile-cutteranim.bat
@@ -0,0 +1,9 @@
+@echo off
+set curdir=%~dp0
+set file_path="%~1"
+set file_path=%file_path:"=%
+set file_path=%file_path:\=\\%
+set file_path=%file_path: =^ %
+set file_name=%~n0
+python "%curdir%%file_name%.py" "%file_path%"
+pause
